7c1decc176cee99a190f88eeeb593eb0bee73675
Jhonel Villanueva <vvjhonel@gmail.com>
2023-03-03 03:53:31 +0800

    Create Kitten API

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,70 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitten }
+    end
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create 
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten
+      flash[:notice] = "Kitten Created!"
+    else
+      flash.now[:alert] = "Creation unsuccessful!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+      flash[:notice] = "Edit Successful!"
+    else
+      flash.now[:alert] = "Edit unsuccessful!"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.destroy
+      redirect_to root_path, status: :see_other
+      flash[:notice] = "Delete Successful!"
+    else
+      flash.now[:alert] = "Deletion unsuccessful!"
+      redirect_to @kitten
+    end
+
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
